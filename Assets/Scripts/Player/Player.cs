696680ba251c0b9a8d3983c0434fada62aa95c3f
oxixes <38050447+oxixes@users.noreply.github.com>
2024-10-03 11:30:57 +0200

    Add player with basic movement and animations

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5.0f;
+
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int horizontalDir = Input.GetAxisRaw("Horizontal") > 0 ? 1 : Input.GetAxisRaw("Horizontal") < 0 ? -1 : 0;
+        int verticalDir = Input.GetAxisRaw("Vertical") > 0 ? 1 : Input.GetAxisRaw("Vertical") < 0 ? -1 : 0;
+
+        animator.SetFloat("XDirection", horizontalDir);
+        animator.SetFloat("YDirection", verticalDir);
+        animator.SetBool("Moving", horizontalDir != 0 || verticalDir != 0);
+
+        Vector2 direction = new Vector2(horizontalDir, verticalDir);
+
+        transform.Translate(direction.normalized * speed * Time.deltaTime);
+    }
+}
