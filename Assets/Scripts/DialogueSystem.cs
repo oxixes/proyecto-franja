4ec0b465e32f3702e8487a091e060d9aaafde6a4
oxixes <38050447+oxixes@users.noreply.github.com>
2024-10-27 12:26:03 +0100

    Add options to the Dialogue System

--- Assets/Scripts/DialogueSystem.cs
+++ Assets/Scripts/DialogueSystem.cs
@@ -21,11 +21,16 @@ public class DialogueSystem : MonoBehaviour
     private float timeSinceLineFinished = 0;
     private int currentCharacterIndex = 0;
     private bool currentLineAccelerated = false;
+    private int currentOptionIndex = 0;
 
     private TextMeshProUGUI dialogueText;
     private TextMeshProUGUI characterNameText;
     private RawImage characterImage;
     private GameObject pressToContinueText;
+
+    private GameObject pressToContinueLayout;
+    private GameObject[] optionLayouts = new GameObject[3];
+    private TextMeshProUGUI[] optionTexts = new TextMeshProUGUI[3];
 
     public DialogueSystem()
     {
@@ -55,19 +60,24 @@ public class DialogueSystem : MonoBehaviour
             return;
         }
 
+        StartDialogueInternal(dialogueId);
+    }
+
+    private bool StartDialogueInternal(string dialogueId)
+    {
         // Find the dialogue data with the given id
         TextAsset dialogueJSON = Resources.Load<TextAsset>("Dialogues/" + dialogueId);
         if (dialogueJSON == null)
         {
             Debug.LogError("Dialogue with id: " + dialogueId + " not found.");
-            return;
+            return false;
         }
 
         currentDialogueData = JsonUtility.FromJson<DialogueData>(dialogueJSON.text);
         if (currentDialogueData == null)
         {
             Debug.LogError("Dialogue with id: " + dialogueId + " could not be parsed.");
-            return;
+            return false;
         }
 
         // Start the dialogue with the given id
@@ -77,6 +87,7 @@ public class DialogueSystem : MonoBehaviour
         gameObject.SetActive(true);
 
         PlayNextDialogueLine();
+        return true;
     }
 
     public bool IsDialogueActive()
@@ -92,12 +103,21 @@ public class DialogueSystem : MonoBehaviour
         characterImage = GameObject.Find("TalkerImage").GetComponent<RawImage>();
         pressToContinueText = GameObject.Find("PressText");
 
+        pressToContinueLayout = GameObject.Find("PressLayout");
+        for (int i = 1; i <= optionLayouts.Length; i++)
+        {
+            optionLayouts[i - 1] = GameObject.Find("Option" + i);
+            optionTexts[i - 1] = GameObject.Find("Option" + i + "Text").GetComponent<TextMeshProUGUI>();
+
+            optionLayouts[i - 1].SetActive(false);
+        }
+
         pressToContinueText.SetActive(false);
         gameObject.SetActive(false);
 
         Debug.Log("Dialogue system is ready!");
 
-        //StartDialogue("TestDialogue");
+        StartDialogue("TestDialogue");
     }
 
     // Update is called once per frame
@@ -108,61 +128,98 @@ public class DialogueSystem : MonoBehaviour
             return;
         }
 
-        bool isDialogueLineFinished = currentCharacterIndex >= currentLineCharacterCount;
-        if (isDialogueLineFinished)
+        if (currentDialogueLine.type.Equals("text"))
         {
-            if (currentDialogueLine.pause)
-            {
-                if (pressToContinueText.activeSelf == false)
-                {
-                    pressToContinueText.SetActive(true);
-                }
+            dialogueText.gameObject.SetActive(true);
+            pressToContinueLayout.SetActive(true);
 
-                // Wait for player input
-                if (Input.GetKeyDown(KeyCode.Space))
+            bool isDialogueLineFinished = currentCharacterIndex >= currentLineCharacterCount;
+            if (isDialogueLineFinished)
+            {
+                if (currentDialogueLine.pause)
+                {
+                    if (pressToContinueText.activeSelf == false)
+                    {
+                        pressToContinueText.SetActive(true);
+                    }
+
+                    // Wait for player input
+                    if (Input.GetKeyDown(KeyCode.Space))
+                    {
+                        timeSinceLastCharacter = 0;
+                        currentCharacterIndex = 0;
+                        timeSinceLineFinished = 0;
+                        currentLineAccelerated = false;
+                        pressToContinueText.SetActive(false);
+                        PlayNextDialogueLine();
+                    }
+                }
+                else if (timeSinceLineFinished >= timeBetweenLines)
                 {
                     timeSinceLastCharacter = 0;
                     currentCharacterIndex = 0;
                     timeSinceLineFinished = 0;
                     currentLineAccelerated = false;
-                    pressToContinueText.SetActive(false);
                     PlayNextDialogueLine();
                 }
-            }
-            else if (timeSinceLineFinished >= timeBetweenLines)
-            {
-                timeSinceLastCharacter = 0;
-                currentCharacterIndex = 0;
-                timeSinceLineFinished = 0;
-                currentLineAccelerated = false;
-                PlayNextDialogueLine();
-            }
 
-            timeSinceLineFinished += Time.deltaTime;
-        }
-        else
-        {
-            float acceleration = 1.0f;
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                currentLineAccelerated = true;
+                timeSinceLineFinished += Time.deltaTime;
             }
-
-            if (currentLineAccelerated)
+            else
             {
-                acceleration = accelerationOnInput;
-            }
-
-            timeSinceLastCharacter += acceleration * Time.deltaTime;
-            if (timeSinceLastCharacter >= 1.0f / textSpeed)
-            {
-                if (currentCharacterIndex < currentLineCharacterCount)
+                float acceleration = 1.0f;
+                if (Input.GetKeyDown(KeyCode.Space))
                 {
-                    dialogueText.text = currentDialogueLine.GetTextUpUntil(currentCharacterIndex + 1);
+                    currentLineAccelerated = true;
                 }
 
-                timeSinceLastCharacter = 0;
-                currentCharacterIndex++;
+                if (currentLineAccelerated)
+                {
+                    acceleration = accelerationOnInput;
+                }
+
+                timeSinceLastCharacter += acceleration * Time.deltaTime;
+                if (timeSinceLastCharacter >= 1.0f / textSpeed)
+                {
+                    if (currentCharacterIndex < currentLineCharacterCount)
+                    {
+                        dialogueText.text = currentDialogueLine.GetTextUpUntil(currentCharacterIndex + 1);
+                    }
+
+                    timeSinceLastCharacter = 0;
+                    currentCharacterIndex++;
+                }
+            }
+        } 
+        else if (currentDialogueLine.type.Equals("options"))
+        {
+            dialogueText.gameObject.SetActive(false);
+            pressToContinueLayout.SetActive(false);
+
+            for (int i = 0; i < currentDialogueLine.options.Length && i < optionLayouts.Length; i++)
+            {
+                if (i == currentOptionIndex)
+                {
+                    optionTexts[i].text = "<color=yellow>> " + currentDialogueLine.options[i].text + "</color>";
+                } else
+                {
+                    optionTexts[i].text = "   " + currentDialogueLine.options[i].text;
+                }
+            }
+
+            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                currentOptionIndex = (currentOptionIndex - 1 + currentDialogueLine.options.Length) % currentDialogueLine.options.Length;
+            }
+            else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                currentOptionIndex = (currentOptionIndex + 1) % currentDialogueLine.options.Length;
+            }
+            else if (Input.GetKeyDown(KeyCode.Space))
+            {
+                currentDialogueLineIndex = -1;
+                characterImage.texture = null;
+                StartDialogueInternal(currentDialogueLine.options[currentOptionIndex].diversion);
             }
         }
     }
@@ -171,7 +228,8 @@ public class DialogueSystem : MonoBehaviour
     {
         currentDialogueLineIndex++;
 
-        if (currentDialogueLineIndex >= currentDialogueData.lines.Length)
+        if (currentDialogueLineIndex >= currentDialogueData.lines.Length ||
+            !currentDialogueData.lines[currentDialogueLineIndex].CheckFormatOk())
         {
             // End of dialogue
             gameObject.SetActive(false);
@@ -183,27 +241,59 @@ public class DialogueSystem : MonoBehaviour
         }
 
         currentDialogueLine = currentDialogueData.lines[currentDialogueLineIndex];
-        currentLineCharacterCount = currentDialogueLine.GetTextCharLength();
 
-        dialogueText.text = "";
-        characterNameText.text = currentDialogueLine.characterName;
-        if (currentDialogueLine.imageAssetId != "")
+        if (currentDialogueLine.type.Equals("text") || currentDialogueLine.type.Equals("diversion"))
         {
-            Texture2D texture = Resources.Load<Texture2D>(currentDialogueLine.imageAssetId);
-            if (texture == null)
+            if (!string.IsNullOrEmpty(currentDialogueLine.imageAssetId))
             {
-                Debug.LogError("Image with id: " + currentDialogueLine.imageAssetId + " not found.");
+                Texture2D texture = Resources.Load<Texture2D>(currentDialogueLine.imageAssetId);
+                if (texture == null)
+                {
+                    Debug.LogError("Image with id: " + currentDialogueLine.imageAssetId + " not found.");
+                }
+                else
+                {
+                    characterImage.texture = texture;
+                }
+
+                characterImage.gameObject.SetActive(true);
             }
             else
             {
-                characterImage.texture = texture;
+                characterImage.gameObject.SetActive(false);
+            }
+        }
+
+        if (currentDialogueLine.type.Equals("text"))
+        {
+            for (int i = 0; i < optionLayouts.Length; i++)
+            {
+                optionLayouts[i].SetActive(false);
             }
 
-            characterImage.gameObject.SetActive(true);
+            currentLineCharacterCount = currentDialogueLine.GetTextCharLength();
+
+            dialogueText.text = "";
+            characterNameText.text = currentDialogueLine.characterName;
         }
-        else
+        else if (currentDialogueLine.type.Equals("options"))
         {
-            characterImage.gameObject.SetActive(false);
+            for (int i = 0; i < currentDialogueLine.options.Length && i < optionLayouts.Length; i++)
+            {
+                optionLayouts[i].SetActive(true);
+            }
+        }
+        else if (currentDialogueLine.type.Equals("diversion"))
+        {
+            currentDialogueLineIndex = -1;
+            characterImage.texture = null;
+            if (!StartDialogueInternal(currentDialogueLine.diversion))
+            {
+                Debug.LogError("Failed to divert to dialogue with id: " + currentDialogueLine.diversion);
+                // End of dialogue
+                gameObject.SetActive(false);
+                isDialogueActive = false;
+            }
         }
     }
 }
