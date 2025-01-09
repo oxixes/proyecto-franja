4d3cae0614055b43f8ea6a804769239bd6575979
oxixes <38050447+oxixes@users.noreply.github.com>
2025-01-10 00:55:38 +0100

    Add sounds. Design combat UI

---
+++ Assets/Scripts/Minigames/CombatMinigame.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class CombatMinigame : Minigame
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        isInMinigame = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
