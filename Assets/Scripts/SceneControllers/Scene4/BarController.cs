9ddc8c8c11d54184e6649e27997220b91ca90534
oxixes <38050447+oxixes@users.noreply.github.com>
2025-01-11 19:09:08 +0100

    Game almost finished

--- Assets/Scripts/SceneControllers/Scene4/BarController.cs
+++ Assets/Scripts/SceneControllers/Scene4/BarController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BarController : MonoBehaviour
 {
@@ -9,6 +10,9 @@ public class BarController : MonoBehaviour
     public GameObject bartender;
     public GameObject jessi;
     public GameObject enamorado;
+    public Vector2 enamoradoPos = new Vector2(-2.01f, 1.85f);
+    public Vector2 enamoradoNextToJessiPos = new Vector2(7.36f, -1.07f);
+    public GameObject fadeOutPanel;
 
     public GameObject player;
 
@@ -58,33 +62,19 @@ public class BarController : MonoBehaviour
             HandleEnamoradoDialogueFinish(null, null, null);
         }
 
-        if (SaveManager.GetInstance().Get<int>("Scene4EnamoradoDecisionChosen") >= 1)
-        {
-            enamoradoIO.dialogueID = "Scene4/EnamoradoFinish";
-            if (SaveManager.GetInstance().Get<int>("Scene4EnamoradoDecisionChosen") == 1)
-            {
-                jessiIO.dialogueID = "Scene4/JessiPiquito";
-            }
-            else if (SaveManager.GetInstance().Get<int>("Scene4EnamoradoDecisionChosen") == 2)
-            {
-                jessiIO.dialogueID = "Scene4/JessiRosas";
-            }
-            else if (SaveManager.GetInstance().Get<int>("Scene4EnamoradoDecisionChosen") == 3)
-            {
-                jessiIO.dialogueID = "Scene4/JessiCopa";
-            }
-            else if (SaveManager.GetInstance().Get<int>("Scene4EnamoradoDecisionChosen") == 4)
-            {
-                jessiIO.dialogueID = "Scene4/JessiCollar";
-            }
-        }
-
         if (SaveManager.GetInstance().Get<int>("Scene4JessiEnamoradoDialogueFinished") == 1)
         {
             jessiIO.dialogueID = "Scene4/JessiRepeat";
         }
 
+        if (SaveManager.GetInstance().Get<int>("Scene4EnamoradoDecisionChosen") >= 1)
+        {
+            enamoradoIO.dialogueID = "Scene4/EnamoradoFinish";
+            jessiIO.dialogueID = "Scene4/EnamoradoYJessiRepeatAfterCopa";
+        }
+
         DialogueSystem.GetInstance().HandleNotification("BarTableDialogueFinished", HandleTableConversationFinishNotification);
+        DialogueSystem.GetInstance().HandleNotification("MinigameFinished", HandleMinigameFinishNotification);
         DialogueSystem.GetInstance().HandleNotification("BartenderDialogueFinished", HandleBartenderFinishNotification);
         DialogueSystem.GetInstance().HandleNotification("JessiDialogueFinished", HandleJessiFinishNotification);
         DialogueSystem.GetInstance().HandleNotification("EnamoradoDialogueFinished", HandleEnamoradoDialogueFinish);
@@ -92,6 +82,7 @@ public class BarController : MonoBehaviour
         DialogueSystem.GetInstance().HandleNotification("EnamoradoCollarNotification", HandleEnamoradoCollarDecision);
         DialogueSystem.GetInstance().HandleNotification("BartenderCopaDialogueFinished", HandleCopaDialogueFinished);
         DialogueSystem.GetInstance().HandleNotification("JessiEnamoradoDialogueFinished", HandleJessiEnamoradoFinish);
+        DialogueSystem.GetInstance().HandleNotification("CutseneFinished", HandleOnCutsceneFinished);
     }
 
     void HandleTableConversationFinishNotification(string dialogueId, string notificationId, string notificationData)
@@ -101,6 +92,23 @@ public class BarController : MonoBehaviour
 
         SaveManager.GetInstance().Set("Scene4BarTableDialogueFinished", 1);
         player.GetComponent<PlayerInventoryManager>().CollectInformation(rayoHasLostInfo);
+
+        if (SaveManager.GetInstance().Get<int>("Scene4EnamoradoDialogueFinished") == 1)
+        {
+            barTableMan1IO.dialogueID = "Scene4/BarTablePostMini";
+            barTableMan2IO.dialogueID = "Scene4/BarTablePostMini";
+            if (dialogueId != null) StartCoroutine(StartMinijuegoDialogue());
+        }
+    }
+
+    void HandleMinigameFinishNotification(string dialogueId, string notificationId, string notificationData)
+    {
+        barTableMan1IO.dialogueID = "Scene4/BarTablePostMini";
+        barTableMan2IO.dialogueID = "Scene4/BarTablePostMini";
+
+        SaveManager.GetInstance().Set("Scene4BarTableDialogueFinished", 1);
+        player.GetComponent<PlayerInventoryManager>().CollectInformation(rayoHasLostInfo);
+        inventory.CollectItem(copaItem);
     }
 
     void HandleBartenderFinishNotification(string dialogueId, string notificationId, string notificationData)
@@ -118,7 +126,8 @@ public class BarController : MonoBehaviour
     void HandleEnamoradoDialogueFinish(string dialogueId, string notificationId, string notificationData)
     {
         enamoradoIO.dialogueID = "Scene4/EnamoradoDecision1";
-        bartenderIO.dialogueID = "Scene4/BartenderCopa";
+        barTableMan1IO.dialogueID = "Scene4/BarTableMinijuego";
+        barTableMan2IO.dialogueID = "Scene4/BarTableMinijuego";
         SaveManager.GetInstance().Set("Scene4EnamoradoDialogueFinished", 1);
     }
 
@@ -128,7 +137,7 @@ public class BarController : MonoBehaviour
         {
             StartCoroutine(StartEnamoradoPiquitoDialogue());
             enamoradoIO.dialogueID = "Scene4/EnamoradoFinish";
-            jessiIO.dialogueID = "Scene4/JessiPiquito";
+            jessiIO.dialogueID = "Scene4/EnamoradoYJessiRepeatAfterCopa";
             SaveManager.GetInstance().Set("Scene4EnamoradoDecisionChosen", 1);
         }
         else if (notificationData == "2" && !inventory.HasItem(flowersItem))
@@ -140,7 +149,7 @@ public class BarController : MonoBehaviour
             StartCoroutine(StartEnamoradoFlowersDialogue());
             enamoradoIO.dialogueID = "Scene4/EnamoradoFinish";
             inventory.RemoveItem(flowersItem);
-            jessiIO.dialogueID = "Scene4/JessiRosas";
+            jessiIO.dialogueID = "Scene4/EnamoradoYJessiRepeatAfterCopa";
             SaveManager.GetInstance().Set("Scene4EnamoradoDecisionChosen", 2);
         }
         else if (notificationData == "3" && !inventory.HasItem(copaItem))
@@ -151,7 +160,7 @@ public class BarController : MonoBehaviour
         {
             StartCoroutine(StartEnamoradoCopaDialogue());
             enamoradoIO.dialogueID = "Scene4/EnamoradoFinish";
-            jessiIO.dialogueID = "Scene4/JessiCopa";
+            jessiIO.dialogueID = "Scene4/EnamoradoYJessiRepeatAfterCopa";
             inventory.RemoveItem(copaItem);
             SaveManager.GetInstance().Set("Scene4EnamoradoDecisionChosen", 3);
         }
@@ -180,7 +189,6 @@ public class BarController : MonoBehaviour
     void HandleCopaDialogueFinished(string dialogueId, string notificationId, string notificationData)
     {
         bartenderIO.dialogueID = "Scene4/BartenderRepeat";
-        inventory.CollectItem(copaItem);
     }
 
     void HandleJessiEnamoradoFinish(string dialogueId, string notificationId, string notificationData)
@@ -202,7 +210,7 @@ public class BarController : MonoBehaviour
     private IEnumerator StartEnamoradoPiquitoDialogue()
     {
         yield return new WaitForSeconds(.1f);
-        DialogueSystem.GetInstance().StartDialogue("Scene4/EnamoradoPiquito");
+        StartCoroutine(FadeOutCutscene("Scene4/CutscenePiquito"));
         yield return null;
     }
 
@@ -216,7 +224,7 @@ public class BarController : MonoBehaviour
     private IEnumerator StartEnamoradoCopaDialogue()
     {
         yield return new WaitForSeconds(.1f);
-        DialogueSystem.GetInstance().StartDialogue("Scene4/EnamoradoCopa");
+        StartCoroutine(FadeOutCutscene("Scene4/CutsceneCopa"));
         yield return null;
     }
 
@@ -230,7 +238,7 @@ public class BarController : MonoBehaviour
     private IEnumerator StartEnamoradoFlowersDialogue()
     {
         yield return new WaitForSeconds(.1f);
-        DialogueSystem.GetInstance().StartDialogue("Scene4/EnamoradoRosas");
+        StartCoroutine(FadeOutCutscene("Scene4/CutsceneFlores"));
         yield return null;
     }
 
@@ -251,7 +259,75 @@ public class BarController : MonoBehaviour
     private IEnumerator StartCleanCollarDialogue()
     {
         yield return new WaitForSeconds(.1f);
-        DialogueSystem.GetInstance().StartDialogue("Scene4/EnamoradoCollar");
+        StartCoroutine(FadeOutCutscene("Scene4/CutsceneCollar"));
         yield return null;
+    }
+
+    private IEnumerator StartMinijuegoDialogue()
+    {
+        yield return new WaitForSeconds(.1f);
+        StartCoroutine(FadeOutCutscene("Scene4/BarTableMinijuego"));
+        yield return null;
+    }
+
+    private void HandleOnCutsceneFinished(string dialogueID, string notificationID, string notificationData)
+    {
+        StartCoroutine(OnCutseneFinished());
+    }
+
+    private IEnumerator FadeOutCutscene(string nextDialogueID)
+    {
+        Minigame.isInMinigame = true;
+        fadeOutPanel.GetComponent<Image>().color = new Color(0, 0, 0, 0);
+        fadeOutPanel.SetActive(true);
+
+        // Animate fade in
+        for (float i = 0; i <= 1; i += Time.deltaTime)
+        {
+            fadeOutPanel.GetComponent<Image>().color = new Color(0, 0, 0, i);
+            yield return null;
+        }
+
+        // TP Enamorado next to Jessi
+        enamorado.transform.localPosition = enamoradoNextToJessiPos;
+
+        // Animate fade out
+        for (float i = 1; i >= 0; i -= Time.deltaTime)
+        {
+            fadeOutPanel.GetComponent<Image>().color = new Color(0, 0, 0, i);
+            yield return null;
+        }
+
+        fadeOutPanel.SetActive(false);
+        Minigame.isInMinigame = false;
+
+        DialogueSystem.GetInstance().StartDialogue(nextDialogueID);
+    }
+
+    private IEnumerator OnCutseneFinished()
+    {
+        Minigame.isInMinigame = true;
+        fadeOutPanel.GetComponent<Image>().color = new Color(0, 0, 0, 0);
+        fadeOutPanel.SetActive(true);
+
+        // Animate fade in
+        for (float i = 0; i <= 1; i += Time.deltaTime)
+        {
+            fadeOutPanel.GetComponent<Image>().color = new Color(0, 0, 0, i);
+            yield return null;
+        }
+
+        // TP Enamorado back to original position
+        enamorado.transform.localPosition = enamoradoPos;
+
+        // Animate fade out
+        for (float i = 1; i >= 0; i -= Time.deltaTime)
+        {
+            fadeOutPanel.GetComponent<Image>().color = new Color(0, 0, 0, i);
+            yield return null;
+        }
+
+        fadeOutPanel.SetActive(false);
+        Minigame.isInMinigame = false;
     }
 }
